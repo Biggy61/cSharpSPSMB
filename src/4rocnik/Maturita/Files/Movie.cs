d3c9093072aab526aa953827eea4adb7f7dec463
Biggy61 <zdanskystepanek@gmail.com>
2025-09-11 15:02:15 +0200

    parser functions

--- src/4rocnik/Maturita/Files/Movie.cs
+++ src/4rocnik/Maturita/Files/Movie.cs
@@ -5,24 +5,24 @@ namespace Files
         public string FilmName { get; set; }
         public string Genre { get; set; }
         public string LeadStudio { get; set; }
-        public string AudienceScore { get; set; }
-        public string Profitability { get; set; }
-        public string RottenTomatoes { get; set; }
+        public int AudienceScore { get; set; }
+        public decimal Profitability { get; set; }
+        public int RottenTomatoes { get; set; }
         public string Gross {get; set;}
-        public string Year { get; set; }
+        public int Year { get; set; }
 
-        public Movie(string filmName, string genre, string leadStudio, string audienceScore, string profitability, string rottenTomatoes, string gross, string year)
+        public Movie(string filmName, string genre, string leadStudio, int audienceScore, decimal profitability, int rottenTomatoes, string gross, int year)
         {
             FilmName = filmName;
             Genre = genre;
             LeadStudio = leadStudio;
-            AudienceScore = audienceScore;
+            AudienceScore =  audienceScore;
             Profitability = profitability;
             RottenTomatoes = rottenTomatoes;
             Gross = gross;
             Year = year;
         }
-        
+
         public override string ToString()
         {
             return $"Name: {this.FilmName}, Genre: {Genre}, LeadStudio: {LeadStudio}, AudienceScore: {AudienceScore} , Profitability: {Profitability}, RottenTomatoes: {RottenTomatoes}, Gross: {Gross}, Year: {Year} ";
--- src/4rocnik/Maturita/Files/Program.cs
+++ src/4rocnik/Maturita/Files/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Files
 {
@@ -8,11 +9,10 @@ namespace Files
   {
     public static void Main(string[] args)
     {
-      List<Movie> movies = GetMovies();
-      foreach (Movie movie in movies)
-      {
-        Console.WriteLine(movie);
-      }
+      WorstRating();
+      BestRating();
+      BestProfitability();
+      WorstProfitability();
     }
 
     public static List<Movie> GetMovies()
@@ -20,27 +20,87 @@ namespace Files
       List<Movie> movies = new List<Movie>();
       using (var reader = new StreamReader(@"./movies.csv"))
       {
+        string headerLine = reader.ReadLine();
         while (!reader.EndOfStream)
         {
           var line = reader.ReadLine();
           var values = line.Split(',');
-          Movie movie = new Movie(values[0], values[1], values[2],  values[3], values[4], values[5],  values[6], values[7]);
+          Movie movie = new Movie(values[0], values[1], values[2],  int.Parse(values[3]), decimal.Parse(values[4]), int.Parse(values[5]) ,  values[6],int.Parse(values[7]));
+          movies.Add(movie);
         }
       }
       return movies;
     }
 
 
-    public static Movie WorstRating()
+    public static void WorstRating()
+    {
+      List<Movie> movies = GetMovies();
+      Movie worstMovie = movies[0];
+      foreach (Movie movie in movies)
+      {
+        var calc = movie.RottenTomatoes;
+        if (worstMovie.RottenTomatoes > calc)
+        {
+          worstMovie = movie;
+        }
+      }
+
+      Console.WriteLine("The worst rated movie is: \n" + worstMovie);
+    }
+    public static void BestRating()
+    {
+      List<Movie> movies = GetMovies();
+      Movie bestMovie = movies[0];
+      foreach (Movie movie in movies)
+      {
+        var calc = movie.RottenTomatoes;
+        if (bestMovie.RottenTomatoes < calc)
+        {
+          bestMovie = movie;
+        }
+      }
+
+      Console.WriteLine("The best rated movie is: \n" + bestMovie);
+    }
+    
+    public static void BestProfitability()
+    {
+      List<Movie> movies = GetMovies();
+      Movie bestProfitableMovie = movies[0];
+      foreach (Movie movie in movies)
+      {
+        var calc = movie.Profitability;
+        if (bestProfitableMovie.Profitability < calc)
+        {
+          bestProfitableMovie = movie;
+        }
+      }
+
+      Console.WriteLine("The most profitable movie is: \n" + bestProfitableMovie);
+    }
+    
+    public static void WorstProfitability()
+    {
+      List<Movie> movies = GetMovies();
+      Movie worstProfitableMovie = movies[0];
+      foreach (Movie movie in movies)
+      {
+        var calc = movie.Profitability;
+        if (worstProfitableMovie.Profitability > calc)
+        {
+          worstProfitableMovie = movie;
+        }
+      }
+
+      Console.WriteLine("The least profitable movie is: \n" + worstProfitableMovie);
+    }
+    
+    public static void WorldWideAverageGrossYearly()
     {
       List<Movie> movies = GetMovies();
 
-      Movie worstMovie;
-
-
-      return worstMovie;
-
-
+      
     }
   }
 }
